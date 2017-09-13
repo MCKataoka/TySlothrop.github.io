4b8e70d1fedc85bd828457dc61b89010f2121a51
Mario C Kataoka <mario.c.kataoka@gmail.com>
2017-09-13 15:52:55 -0400

    Adding the style and js  files externally

---
+++ Floating.js
@@ -0,0 +1,69 @@
+var bar1 = document.getElementById("bar1");
+//add the class that makes the bars float when the mouse hovers over the bars
+bar1.addEventListener(
+  "mouseover",
+  function() {
+    bar1.classList.add("floating");
+
+    setTimeout(function() {
+      bar1.classList.remove("floating");
+    }, 12000);
+  },
+  false
+);
+
+var bar2 = document.getElementById("bar2");
+
+bar2.addEventListener(
+  "mouseover",
+  function() {
+    bar2.classList.add("floating");
+
+    setTimeout(function() {
+      bar2.classList.remove("floating");
+    }, 12000);
+  },
+  false
+);
+
+var bar3 = document.getElementById("bar3");
+
+bar3.addEventListener(
+  "mouseover",
+  function() {
+    bar3.classList.add("floating");
+
+    setTimeout(function() {
+      bar3.classList.remove("floating");
+    }, 12000);
+  },
+  false
+);
+
+var bar4 = document.getElementById("bar4");
+
+bar4.addEventListener(
+  "mouseover",
+  function() {
+    bar4.classList.add("floating");
+
+    setTimeout(function() {
+      bar4.classList.remove("floating");
+    }, 12000);
+  },
+  false
+);
+
+var bar5 = document.getElementById("bar5");
+
+bar5.addEventListener(
+  "mouseover",
+  function() {
+    bar5.classList.add("floating");
+
+    setTimeout(function() {
+      bar5.classList.remove("floating");
+    }, 12000);
+  },
+  false
+);
